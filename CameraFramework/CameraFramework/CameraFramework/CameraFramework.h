b7bc3ab2cf57c4f03f00c5493b77590f6e675235
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-01-17 21:30:56 +0100

    First commit

---
+++ CameraFramework/CameraFramework/CameraFramework/CameraFramework.h
@@ -0,0 +1,18 @@
+//
+//  CameraFramework.h
+//  CameraFramework
+//
+//  Created by Miguel Gallego on 17/1/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CameraFramework.
+FOUNDATION_EXPORT double CameraFrameworkVersionNumber;
+
+//! Project version string for CameraFramework.
+FOUNDATION_EXPORT const unsigned char CameraFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CameraFramework/PublicHeader.h>
+
+
---
+++ SampleAppForCamFramework/SampleAppForCamFramework/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  MainVC.swift
+//  SampleAppForCamFramework
+//
+//  Created by Miguel Gallego on 17/1/25.
+//
+
+import UIKit
+import CameraFramework
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
